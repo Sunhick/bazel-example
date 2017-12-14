091787c11f27703f87640dfd8dd27f55a185478b
Sunil <sunhick@gmail.com>
2017-12-14 15:19:49 -0800

    add git repository as deps

--- lion/main/starter.cc
+++ lion/main/starter.cc
@@ -4,11 +4,13 @@
 #include "graphics/graphics.h"
 #include "main/common.h"
 #include "session/session.h"
+#include "gnu/profile.h"
 
 #include <iostream>
 
 using namespace std;
 using namespace bazel101::console;
+using namespace bazel101::profile;
 
 int main(UNUSED int argc, UNUSED char **argv) {
   Console writer;
@@ -16,6 +18,9 @@ int main(UNUSED int argc, UNUSED char **argv) {
 
   graphics_init();
   start_session();
+
+  Profile pf;
+  pf.describe();
 
   return 0;
 }
